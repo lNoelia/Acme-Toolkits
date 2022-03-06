4c4c2f4e0b345b49844e4e8164c8dee7855c925d
Josdelsan <joseM_DS@hotmail.com>
2022-03-06 17:33:44 +0100

    Task 028: Done

---
+++ src/main/java/acme/entities/Patronage.java
@@ -0,0 +1,96 @@
+package acme.entities;
+
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Patronage extends AbstractEntity{
+	
+	// Serialisation identifier -----------------------------------------------
+	
+	private static final long serialVersionUID = 1L;
+	
+	// Attributes -------------------------------------------------------------
+	
+	@NotNull
+	protected PatronageStatus status;
+	
+	@NotNull
+	@Column(unique = true)
+	@Pattern(regexp = "^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	protected String code;
+	
+	@NotBlank
+	@Length(max = 255)
+	protected String legal;
+	
+	@Min(0)
+	protected int budget;
+	
+	@URL
+	protected String link;
+	
+	@Temporal(TemporalType.DATE)
+	@Past
+	@NotNull
+	protected Date creationDate;
+	
+	@Temporal(TemporalType.DATE)
+	@NotNull
+	protected Date startDate;
+	
+	@Temporal(TemporalType.DATE)
+	@NotNull
+	protected Date endDate;
+	
+	// Methods ----------------------------------------------------------------
+	
+	@PrePersist
+	protected void prePersist() throws Exception {
+		if(this.creationDate instanceof Date) {
+			this.creationDate = new Date();
+			this.preUpdate();
+		}
+	}
+	
+	@PreUpdate
+	protected void preUpdate() throws Exception {
+		final long creationDateEpoch = this.creationDate.toInstant().toEpochMilli();
+		final long startDateEpoch = this.startDate.toInstant().toEpochMilli();
+		final long endDateEpoch = this.endDate.toInstant().toEpochMilli();
+
+		final long startingDelayDays = TimeUnit.MILLISECONDS.toDays(startDateEpoch - creationDateEpoch);
+		final long totalDurationDays = TimeUnit.MILLISECONDS.toDays(endDateEpoch - startDateEpoch);
+
+		if(startingDelayDays < 30l) {
+			throw new Exception("The start of the patronage must be at least 30 days after de patronage was created.");
+		}
+		
+		if(totalDurationDays < 30l) {
+			throw new Exception("The duration of the patronage must be at least 30 days long.");
+		}
+		
+	}
+	
+}
