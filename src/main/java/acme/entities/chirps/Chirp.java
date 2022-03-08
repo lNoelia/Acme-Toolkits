e3ff8667fbac09071375d6b1f5c058952b957601
noelopdur <noelopdur@alum.us.es>
2022-03-08 19:09:59 +0100

    Task 023:done

---
+++ src/main/java/acme/entities/chirps/Chirp.java
@@ -0,0 +1,47 @@
+package acme.entities.chirps;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Past;
+
+import org.hibernate.validator.constraints.Length;
+
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Chirp  extends AbstractEntity {
+	//A chirp is an informal short message
+	
+	protected static final long serialVersionUID = 1L;
+	
+	// Attributes
+		@Temporal(TemporalType.TIMESTAMP)
+		@Past
+		@NotNull
+		protected Date creationDate;
+		
+		@NotBlank
+		@Length(max = 100)
+		protected String title;
+		
+		@NotBlank
+		@Length(max = 100)
+		protected String author;
+		
+		@NotBlank
+		@Length(max = 255)
+		protected String body;
+		
+		@Email
+		protected String email;
+}
