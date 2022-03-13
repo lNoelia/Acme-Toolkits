7fe07bf8e76906bc91ea7cd5ead34349a357fb5e
noelopdur <73660773+lNoelia@users.noreply.github.com>
2022-03-13 19:17:08 +0100

    Fix AbstractRol to AbstractRole

--- src/main/java/acme/roles/Inventor.java
+++ src/main/java/acme/roles/Inventor.java
@@ -20,7 +20,7 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Inventor extends AbstractRol  {
+public class Inventor extends AbstractRole  {
 
 	// Serialisation identifier -----------------------------------------------
 
