df089ece40fb777afdbcdefa193d5f35516b7030
Rick3600 <ricnadgar@alum.us.es>
2022-03-13 17:43:29 +0100

    Task 26: Constraints fix

--- src/main/java/acme/entities/Tool.java
+++ src/main/java/acme/entities/Tool.java
@@ -4,9 +4,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
 
 import acme.framework.entities.AbstractEntity;
 import lombok.Getter;
@@ -23,15 +25,16 @@ public class Tool extends AbstractEntity{
 	// Attributes -------------------------------------------------------------
 
 	@NotBlank
-	@Length(max=101)
+	@Length(max=100)
 	protected String name;
 	
 	@Pattern(regexp="^[AZ]{3}-[0-9]{3}(-[AZ])?$")
 	@Column(unique = true)
+	@NotNull
 	protected String code;
 	
 	@NotBlank
-	@Length(max=101)
+	@Length(max=100)
 	protected String technology;
 	
 	@NotBlank
@@ -39,7 +42,9 @@ public class Tool extends AbstractEntity{
 	protected String description;
 	
 	@Min(0)
+	@NotNull
 	protected Integer price;
 	
+	@URL
 	protected String link;
 }
