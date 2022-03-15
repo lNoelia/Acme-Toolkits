f9e08adb0ca65fc4c63236fe41060c55397cdfb6
sarcrudua <saraDP2@sara-laptop.home>
2022-03-15 14:55:12 +0100

    Task 027: in progress

---
+++ src/main/java/acme/entities/toolkits/Toolkit.java
@@ -0,0 +1,63 @@
+package acme.entities.toolkits;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.URL;
+
+import acme.entities.components.Component;
+import acme.entities.tools.Tool;
+import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Toolkit extends AbstractEntity {
+
+	// Serialisation identifier -----------------------------------------------
+
+	protected static final long	serialVersionUID	= 1L;
+
+	// Attributes -------------------------------------------------------------
+
+	@Pattern(regexp="^[A-Z]{3}-[0-9]{3}(-[A-Z])?$")
+	@Column(unique = true)
+	protected String code;
+	
+	@NotBlank
+	@Length(min=0, max=100)
+	protected String title;
+	
+	@NotBlank
+	@Length(min=0, max=255)
+	protected String description;
+	
+	@NotBlank
+	@Length(min=0, max=255)
+	protected String assemblyNotes;
+	
+	@URL
+	protected String link;
+	
+	// Relationships ----------------------------------------------------------
+
+	@NotNull
+	@Valid
+	@OneToMany
+	protected Component			component;
+
+	@NotNull
+	@Valid
+	@ManyToOne(optional = false)
+	protected Tool				tool;
+
+}
