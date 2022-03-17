77ba55f4ccd4d515b7667a937447da8ed7649a8d
sarcrudua <saraDP2@sara-laptop.home>
2022-03-17 23:50:23 +0100

    Task 038: done

---
+++ src/main/java/acme/entities/worksIn/WorksIn.java
@@ -0,0 +1,41 @@
+package acme.entities.worksIn;
+
+import javax.persistence.ManyToOne;
+import javax.validation.Valid;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import acme.entities.artefact.Artefact;
+import acme.entities.toolkits.Toolkit;
+import acme.framework.entities.AbstractEntity;
+
+public class WorksIn extends AbstractEntity{
+	
+		// Serialisation identifier -----------------------------------------------
+
+		protected static final long	serialVersionUID	= 1L;
+
+		// Attributes -------------------------------------------------------------
+
+		@NotBlank
+		protected Integer idToolkit;
+		
+		@NotBlank
+		protected Integer idArtefact;
+		
+		@NotNull
+		protected ArtefactType type;
+		
+		// Relationships ----------------------------------------------------------
+		
+		@NotNull
+		@Valid
+		@ManyToOne(optional = false)
+		protected Artefact artefact;
+		
+		@NotNull
+		@Valid
+		@ManyToOne(optional = false)
+		protected Toolkit toolkit;
+		
+}
