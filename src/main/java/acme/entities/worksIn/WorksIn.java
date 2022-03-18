49db0e88e190fb530f49f6a1ce5e8336a3de733c
sarcrudua <saraDP2@sara-laptop.home>
2022-03-18 10:46:54 +0100

    Task 038: done

--- src/main/java/acme/entities/worksIn/WorksIn.java
+++ src/main/java/acme/entities/worksIn/WorksIn.java
@@ -1,5 +1,6 @@
 package acme.entities.worksIn;
 
+import javax.persistence.Entity;
 import javax.persistence.ManyToOne;
 import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
@@ -8,7 +9,13 @@ import javax.validation.constraints.NotNull;
 import acme.entities.artefact.Artefact;
 import acme.entities.toolkits.Toolkit;
 import acme.framework.entities.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
 
+
+@Entity
+@Getter
+@Setter
 public class WorksIn extends AbstractEntity{
 	
 		// Serialisation identifier -----------------------------------------------
