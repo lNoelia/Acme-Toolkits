82e51302a325631fc6bb91340db41dc9387522f9
noelopdur <noelopdur@alum.us.es>
2022-04-07 22:05:49 +0200

    Task 055:done

---
+++ src/main/java/acme/features/inventor/artefact/InventorArtefactController.java
@@ -0,0 +1,30 @@
+package acme.features.inventor.artefact;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import acme.entities.artefact.Artefact;
+import acme.framework.controllers.AbstractController;
+import acme.roles.Inventor;
+
+@Controller
+public class InventorArtefactController extends AbstractController<Inventor, Artefact>{
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected InventorArtefactListService		listService;
+
+	@Autowired
+	protected InventorArtefactShowService		showService;
+
+	// Constructors -----------------------------------------------------------
+
+	@PostConstruct
+	protected void initialise() {
+		super.addCommand("list", "list", this.listService);
+		super.addCommand("show", "show", this.showService);
+	}
+}
