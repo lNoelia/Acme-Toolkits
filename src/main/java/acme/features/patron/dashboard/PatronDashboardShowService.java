88ec0ea92af4e27af74f50878b216f818ffdf4ae
Rick3600 <ricnadgar@alum.us.es>
2022-04-14 10:42:30 +0200

    Task 061: Realizadas tres primeras consultas basicas

---
+++ src/main/java/acme/features/patron/dashboard/PatronDashboardShowService.java
@@ -0,0 +1,79 @@
+/*
+ * AdministratorDashboardShowService.java
+ *
+ * Copyright (C) 2012-2022 Rafael Corchuelo.
+ *
+ * In keeping with the traditional purpose of furthering education and research, it is
+ * the policy of the copyright owner to permit non-commercial use and redistribution of
+ * this software. It has been tested carefully, but it is not guaranteed for any particular
+ * purposes. The copyright owner does not offer any warranties or representations, nor do
+ * they accept any liabilities with respect to them.
+ */
+
+package acme.features.patron.dashboard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.forms.PatronDashboard;
+import acme.framework.components.models.Model;
+import acme.framework.controllers.Request;
+import acme.framework.services.AbstractShowService;
+import acme.roles.Patron;
+
+@Service
+public class PatronDashboardShowService implements AbstractShowService<Patron, PatronDashboard> {
+
+	// Internal state ---------------------------------------------------------
+
+	@Autowired
+	protected PatronDashboardRepository repository;
+
+	// AbstractShowService<Patron, PatronDashboard> interface ----------------
+
+
+	@Override
+	public boolean authorise(final Request<PatronDashboard> request) {
+		assert request != null;
+
+		return true;
+	}
+
+	@Override
+	public PatronDashboard findOne(final Request<PatronDashboard> request) {
+		assert request != null;
+
+		PatronDashboard result;
+		int totalNumberOfProposedPatronages;
+		int totalNumberOfAcceptedPatronages;
+		int totalNumberOfDeniedPatronages;
+		
+		
+		totalNumberOfProposedPatronages=this.repository.totalNumberOfProposedPatronages();
+		totalNumberOfAcceptedPatronages=this.repository.totalNumberOfAcceptedPatronages();
+		totalNumberOfDeniedPatronages= this.repository.totalNumberOfDeniedPatronages();
+		
+
+		result = new PatronDashboard();
+		result.setTotalNumberOfProposedPatronages(totalNumberOfProposedPatronages);
+		result.setTotalNumberOfAcceptedPatronages(totalNumberOfAcceptedPatronages);
+		result.setTotalNumberOfDeniedPatronages(totalNumberOfDeniedPatronages);
+
+		return result;
+	}
+
+	@Override
+	public void unbind(final Request<PatronDashboard> request, final PatronDashboard entity, final Model model) {
+		assert request != null;
+		assert entity != null;
+		assert model != null;
+
+		
+		
+		
+		request.unbind(entity, model,"totalNumberOfProposedPatronages",
+			"totalNumberOfAcceptedPatronages","totalNumberOfDeniedPatronages"//
+			);
+	}
+
+}
