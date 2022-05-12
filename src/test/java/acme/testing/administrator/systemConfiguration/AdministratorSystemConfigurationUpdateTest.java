eab20726055925c7c8f5c1ad7a279c8ffc9a1217
Rick3600 <ricnadgar@alum.us.es>
2022-05-12 21:30:24 +0200

    Task 110: Fix

--- src/test/java/acme/testing/administrator/systemConfiguration/AdministratorSystemConfigurationUpdateTest.java
+++ src/test/java/acme/testing/administrator/systemConfiguration/AdministratorSystemConfigurationUpdateTest.java
@@ -1,12 +1,13 @@
 package acme.testing.administrator.systemConfiguration;
 
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import acme.testing.TestHarness;
 
-public class AdministratorSystemConfigurationCreateTest extends TestHarness{
+public class AdministratorSystemConfigurationUpdateTest extends TestHarness{
 
 	@ParameterizedTest
 	@CsvFileSource(resources = "/administrator/system-configuration/create.csv", encoding = "utf-8", numLinesToSkip = 1)
@@ -36,7 +37,7 @@ public class AdministratorSystemConfigurationCreateTest extends TestHarness{
 	}
 	
 	@ParameterizedTest
-	@CsvFileSource(resources = "/administrator/system-configuration/create.csv", encoding = "utf-8", numLinesToSkip = 1)
+	@CsvFileSource(resources = "/administrator/system-configuration/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
 	public void negativeTest(final int recordIndex, final String acceptedCurrencies, final String systemCurrency, final String strongSpamWords, final String strongSpamThreshold, final String weakSpamWords, final String weakSpamThreshold) {
 		super.signIn("administrator", "administrator");
@@ -44,7 +45,7 @@ public class AdministratorSystemConfigurationCreateTest extends TestHarness{
 		super.clickOnMenu("Administrator", "System configuration");
 		super.checkFormExists();
 		super.fillInputBoxIn("acceptedCurrencies", acceptedCurrencies);
-		super.fillInputBoxIn("systemCurrency", "");
+		super.fillInputBoxIn("systemCurrency", systemCurrency);
 		super.fillInputBoxIn("strongSpamWords", strongSpamWords);
 		super.fillInputBoxIn("strongSpamThreshold", strongSpamThreshold);
 		super.fillInputBoxIn("weakSpamWords", weakSpamWords);
@@ -52,5 +53,15 @@ public class AdministratorSystemConfigurationCreateTest extends TestHarness{
 		super.clickOnSubmit("Update");
 		
 		super.checkErrorsExist();
+	}
+	
+
+	@Test
+	@Order(30)
+	public void hackingTest() {
+		super.checkNotLinkExists("Account");
+		super.navigate("/administrator/system-configuration/update");
+		super.checkPanicExists();
+		
 	}
 }
