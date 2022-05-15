5722c99c243b573f60d8433c229967a82ce5ed7d
rosmolarr <rosmolarr@alum.us.es>
2022-05-15 22:04:15 +0200

    Task 112: Things

--- src/test/java/acme/testing/any/chirp/AnyChirpListTest.java
+++ src/test/java/acme/testing/any/chirp/AnyChirpListTest.java
@@ -1,16 +1,38 @@
 package acme.testing.any.chirp;
 
+import java.util.Calendar;
+import java.util.Collection;
+import java.util.Date;
+
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import acme.entities.chirps.Chirp;
+import acme.features.any.chirp.AnyChirpRepository;
+import acme.framework.helpers.FactoryHelper;
 import acme.testing.TestHarness;
 
 public class AnyChirpListTest extends TestHarness{
 
 	// Lifecycle management ---------------------------------------------------
 
+	@Autowired
+	private AnyChirpRepository repository;
+	
 	// Test cases -------------------------------------------------------------
+	
+	@Override
+	@BeforeAll
+	public void beforeAll() {
+	    super.beforeAll();
+	    FactoryHelper.autowire(this);
+	    this.patchChirps();
+	}
+	
+	
 	@ParameterizedTest
 	@CsvFileSource(resources = "/any/chirp/list.csv", encoding = "utf-8", numLinesToSkip = 1)
 	@Order(10)
@@ -46,6 +68,40 @@ public class AnyChirpListTest extends TestHarness{
 		super.checkColumnHasValue(recordIndex, 4, email);
 	}
 	
+	protected void patchChirps() {
+	    Collection<Chirp> chirps;
+	    Date moment;
+
+	    chirps = this.repository.findChirpsToPatch();
+	    for (final Chirp chirp : chirps) {
+	        moment = this.adjustMoment(chirp.getCreationDate());
+	        chirp.setCreationDate(moment);
+	        this.repository.save(chirp);
+	    }
+	}
 	
+	protected Date adjustMoment(final Date currentMoment) {
+		assert currentMoment != null;
+
+		Date result;
+		Calendar calendar;
+		int day;
+
+		calendar = Calendar.getInstance();
+
+		calendar.setTime(currentMoment);
+		day = calendar.get(Calendar.DAY_OF_MONTH);
+
+		calendar.setTime(new Date());
+		calendar.set(Calendar.HOUR, 00);
+		calendar.set(Calendar.MINUTE, 00);
+		calendar.set(Calendar.SECOND, 00);
+		calendar.set(Calendar.MILLISECOND, 000);
+		calendar.add(Calendar.DAY_OF_MONTH, -day);
+
+		result = calendar.getTime();
+
+		return result;
+	}
 	
 }
