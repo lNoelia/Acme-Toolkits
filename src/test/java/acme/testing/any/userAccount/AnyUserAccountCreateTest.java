9f37c7ddd481971a698a16eb987986713732128e
Josdelsan <joseM_DS@hotmail.com>
2022-05-21 14:31:14 +0200

    Task 117/R1: Done

--- src/test/java/acme/testing/any/userAccount/AnyUserAccountCreateTest.java
+++ src/test/java/acme/testing/any/userAccount/AnyUserAccountCreateTest.java
@@ -1,6 +1,7 @@
 package acme.testing.any.userAccount;
 
 import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
@@ -23,17 +24,24 @@ public class AnyUserAccountCreateTest extends TestHarness {
 		@ParameterizedTest
 		@CsvFileSource(resources = "/any/user-account/create-negative.csv", encoding = "utf-8", numLinesToSkip = 1)
 		@Order(20)
-		public void negativeTest(final String username, final String password, final String confirmation, final String name, final String surname, final String email) {
+		public void negativeTest(final String username, final String password, final String confirmation, final String name, final String surname, final String email, final String checkbox) {
 			super.clickOnMenu("Sign up");	
 			super.fillInputBoxIn("username", username);
 			super.fillInputBoxIn("password", password);
-			super.fillInputBoxIn("confirmation", password);
+			super.fillInputBoxIn("confirmation", confirmation);
 			super.fillInputBoxIn("identity.name", name);
 			super.fillInputBoxIn("identity.surname", surname);
 			super.fillInputBoxIn("identity.email", email);		
-			super.fillInputBoxIn("accept", "true");
+			super.fillInputBoxIn("accept", checkbox);
 			super.clickOnSubmit("Sign up");
 			
 			super.checkErrorsExist();
+		}
+		
+		@Test
+		@Order(30)
+		public void hackingTest() {
+			// HINT: there's no hacking test case for this create, this feature
+			// HINT+ is available for all principals.
 		}
 }
